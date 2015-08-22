c1a316eb5b0be0ad02d60cf9bab8111c0ae56f57
xzenggit <xzengemail@gmail.com>
2015-08-22 12:52:33 -0400

    resize pics

--- img/portfolio/resize_fig.bash
+++ img/portfolio/resize_fig.bash
@@ -1,5 +1,13 @@
 #!/bin/bash
-fname=ECS-sediment.png
-convert $fname -resize 900x650 -gravity center -extent 900x650 $fname
+# This is a bash script.
+# Use ImageMagick to resize a picture to certain size with locked ratio.
+# The margins will be filled with white color in default. 
+# You can also use `-backgroud` parameter to specify the filled color.
+# Here, 900x650 is resized picture size in pixels. 
+# `-gravity` specifies the position of origional picture in resized picture.
+# `-extent` is used to filled the margins in resized pictures. 
+
+fname=LC-SOM.png
+convert $fname -trim -resize 900x650 -gravity center -extent 900x650 $fname
 
 
